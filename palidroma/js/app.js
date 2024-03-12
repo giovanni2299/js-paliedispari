626dc64fd499e62948962549671a8f7417a87677
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-03-12 16:19:48 +0100

    ADD:palindroma exercise

--- palidroma/js/app.js
+++ palidroma/js/app.js
@@ -7,10 +7,43 @@
 // deve ritornare false se la parola non è palindroma
 // alla fine stampate in console un messaggio per dire all’utente il risultato del controllo
 
-const word = prompt('choose your word')
+const word = prompt('choose your word');
 
-// console.log(word)
+// console.log(word);
 
-function ispalindrome(word){
-    
+const arrayRow = []; // uno parole dritte 
+
+for(let i = 0; i < word.length; i++){
+    // console.log(word[i])
+    const wordIndex = word[i];
+    arrayRow.push(wordIndex);
 }
+console.log(arrayRow);
+
+
+const arrayRowReverse = []; //parole al rovescio 
+
+for(let j = word.length - 1; j >= 0 ; j--){
+    // console.log(word[j]);
+    const wordIndexReverse = word[j];
+    arrayRowReverse.push(wordIndexReverse);
+}
+console.log(arrayRowReverse);
+
+function isPalindrome(word){
+    //osso
+    //anna
+    //non
+    //radar
+    //oronarono
+
+    if(arrayRow === arrayRowReverse){
+        console.log('the word is palindrome');
+        return true;
+    } else{
+        console.log('the word is not palindrome');
+        return false;
+    }
+}
+const wordLenght = isPalindrome(word);
+console.log(wordLenght);
