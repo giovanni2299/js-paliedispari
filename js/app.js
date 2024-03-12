f85477c294caed60d32a771603421baaa523bdea
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-03-12 14:02:08 +0100

    ADD:copied the task on js paper

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// console.log('ciao mamma')
+
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma:
+// la funzione deve ritornare true se la parola è palindroma
+// deve ritornare false se la parola non è palindroma
+// alla fine stampate in console un messaggio per dire all’utente il risultato del controllo
+
